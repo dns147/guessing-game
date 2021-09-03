3f36b9d4b03a23d1071c9fe12b2a28b7adff39a5
Denis <rarden@ya.ru>
2021-09-03 14:56:02 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,38 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number) {
+        this.range = [];
+        this.low = null;
+        this.high = null;
+        this.mid = null;
+        this.num = number;
+    }
 
     setRange(min, max) {
+        for (let i = min; i <= max; i++) {
+            this.range.push(i);
+        }
 
+        this.low = 0;
+        this.high = this.range.length - 1;
     }
 
     guess() {
+        this.mid = Math.round((this.low + this.high) / 2);
+        let gues = this.range[this.mid];
 
+        return gues;
     }
 
     lower() {
-
+        if (this.guess() > this.num) {
+            this.high = this.mid;
+        }
     }
 
     greater() {
-
+        if (this.guess() < this.num) {
+            this.low = this.mid;
+        }
     }
 }
 
